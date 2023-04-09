5174626c9fc092e5bf6f8ae52325ac7b882ff4f0
RafaSauBlas <RafaSauBlas@hotmail.com>
2023-04-09 16:17:25 -0600

    Se corrige un error en las pantallas de los codigos

--- resources/views/Mails/mailcodigo.blade.php
+++ resources/views/Mails/mailcodigo.blade.php
@@ -4,9 +4,9 @@
     <title>Rafael INC incorporated asocieshon company</title>
 </head>
 <body>
-<h4><p> Su codigo para {{ $utilidad }}: </p></h4>
-<p>{{ $code }}</p>
+<h4><p> Para poder continuar se ha generado un codigo de acceso, el cual puede ver accediendo al siguiente enlace: </p></h4>
+<p><a href="{{ $code }}">{{ $code }}</a></p>
                                         
-    <b><p>Este codigo solo pude ser utilizado por usted y tiene un tiempo limite de 10 minutos</p></b>
+    <b><p>POR FAVOR NO COMPARATA EL CODIGO CON NADIE!!!</p></b>
 </body>
 </html>
---
+++ resources/views/Mails/mailutilidad.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rafael INC incorporated asocieshon company</title>
+</head>
+<body>
+<h4><p> Su codigo para {{ $utilidad }}: </p></h4>
+<p>{{ $code }}</p>
+                                        
+    <b><p>Este codigo solo pude ser utilizado por usted y tiene un tiempo limite de 10 minutos</p></b>
+</body>
+</html>
