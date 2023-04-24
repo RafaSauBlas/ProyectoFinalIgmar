64a6d44f48f9188377b2c917792d1d8633e63d7b
RafaSauBlas <RafaSauBlas@hotmail.com>
2023-04-24 11:59:17 -0600

    Pantalla de error

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,11 @@
+<body>
+<br>
+<br>
+<br>
+<center> <h1>Se ha perdido la conexión con el servidor de base de datos, por favor intentelo mas tarde :).</h1> </center>
+<center>
+<img src="https://blackdogvideo.ca/wp-content/uploads/2021/07/a2163b7fcaea9baac139cf5c4f9fb943.jpg" width=540 height=540>
+</center>
+
+<center> <h4>Si el problema no se soluciona intente reiniciando su equipo.</h4> </center>
+</body>
